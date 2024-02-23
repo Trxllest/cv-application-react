0bcecb2336273d22859c9c9beb7de8d6aee8b701
David Olona <damilear@outlook.com>
2024-02-23 17:19:54 -0500

    Add Inputs

---
+++ src/components/Employment.jsx
@@ -0,0 +1,18 @@
+function Employment() {
+  return (
+    <form>
+      <label htmlFor="">Company: </label>
+      <input type="text" />
+      <label htmlFor="">Position Title: </label>
+      <input type="text" />
+      <label htmlFor="">Description: </label>
+      <textarea ></textarea>
+      <label htmlFor="">Start: </label>
+      <input type="date" />
+      <label htmlFor="">End: </label>
+      <input type="date" />
+    </form>
+  );
+}
+
+export default Employment;
