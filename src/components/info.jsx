66b6208e37650b1de0e9c23ea4d48bec86e72f5d
David Olona <damilear@outlook.com>
2024-02-23 17:20:19 -0500

    Add person state

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,14 +1,61 @@
+import { useState } from "react";
+
 function Info() {
-    return (
-        <form>
-            <label htmlFor="">Name:{' '}</label>
-            <input type="text" />
-            <label htmlFor="">Email:{' '}</label>
-            <input type="email" />
-            <label htmlFor="">Phone Number:{' '}</label>
-            <input type="tel" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
-        </form>
-    );
+  const [person, setPerson] = useState({
+    name: "John Doe",
+    email: "example@gmail.com",
+    phone: "123-456-7890",
+  });
+
+  const handleNameChange = (e) => {
+    setPerson((prevPerson) => ({
+      ...prevPerson,
+      name: e.target.value
+    }));
+    console.log('changed')
+  };
+
+  const handleEmailChange = (e) => {
+    setPerson((prevPerson) => ({
+      ...prevPerson,
+      email: e.target.value
+    }));
+  };
+
+  const handlePhoneChange = (e) => {
+    setPerson((prevPerson) => ({
+      ...prevPerson,
+      phone: e.target.value
+    }));
+  };
+
+  return (
+    <form>
+      <label htmlFor="">Name: </label>
+      <input
+        type="text"
+        value={person.name}
+        placeholder={person.name}
+        onChange={handleNameChange}
+      />
+      
+      <label htmlFor="">Email: </label>
+      <input
+        type="email"
+        value={person.email}
+        placeholder={person.email}
+        onChange={handleEmailChange}
+      />
+      <label htmlFor="">Phone Number: </label>
+      <input
+        type="tel"
+        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+        value={person.phone}
+        placeholder={person.phone}
+        onChange={handlePhoneChange}
+      />
+    </form>
+  );
 }
 
-export default Info
+export default Info;
