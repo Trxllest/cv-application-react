ac86687731b0126aad48895103b56efb8c1bbb77
David Olona <damilear@outlook.com>
2024-02-28 22:14:56 -0500

    Add console logs for debugging

--- src/components/Education/EduForm.jsx
+++ src/components/Education/EduForm.jsx
@@ -1,59 +1,71 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 
-function EduForm({prevList, setList, isSaved, setIsSaved}) {
+function EduForm({ prevList, setList, isSaved, setIsSaved }) {
   const [newEdu, setNewEdu] = useState({
     school: "",
     eduTitle: "",
     eduStart: null,
     eduEnd: null,
-    eduId: null
-  })
+    eduId: "",
+  });
 
   let handleSchool = (e) => {
-     setNewEdu((newEdu) => ({
-      ...newEdu, school: e.target.value
-     }))
-  }
-  let handleTitle = (e) => {
-     setNewEdu((newEdu) => ({
-      ...newEdu, eduTitle: e.target.value
-     }))
-  }
-  let handleStart = (e) => {
-     setNewEdu((newEdu) => ({
-      ...newEdu, eduStart: e.target.value
-     }))
-  }
-  let handleEnd = (e) => {
-     setNewEdu((newEdu) => ({
-      ...newEdu, eduEnd: e.target.value
-     }))
-  }
-
-  let saveEdu = () => {
-    if ((newEdu.school === '') || (newEdu.eduTitle === '') || !newEdu.eduStart
-        || !newEdu.eduEnd) {
-          alert("Missing Fields")
-          return
-        }
-    if (newEdu.eduEnd < newEdu.eduStart) {
-      alert('End date must be after start date')
-      setNewEdu((newEdu) => ({
-        ...newEdu,
-        eduEnd: null
-      }));
-      return
-    }
-
     setNewEdu((newEdu) => ({
       ...newEdu,
-      eduId: uuidv4()
+      school: e.target.value,
     }));
-    setList([...prevList, newEdu])
-    console.log(`Education saved \n ${newEdu}`);
-  }
+  };
+  let handleTitle = (e) => {
+    setNewEdu((newEdu) => ({
+      ...newEdu,
+      eduTitle: e.target.value,
+    }));
+  };
+  let handleStart = (e) => {
+    setNewEdu((newEdu) => ({
+      ...newEdu,
+      eduStart: e.target.value,
+    }));
+  };
+  let handleEnd = (e) => {
+    setNewEdu((newEdu) => ({
+      ...newEdu,
+      eduEnd: e.target.value,
+    }));
+  };
+  let handleID = async () => {
+    const updatedEduId = crypto.randomUUID();
+    console.log(`UUID is: ${updatedEduId}`);
+    setNewEdu((newEdu) => ({
+      ...newEdu,
+      eduId: updatedEduId,
+    }));
+    console.log(`Education saved \n ${JSON.stringify(newEdu)}`);
+  };
 
+  let saveEdu = async () => {
+    if (
+      newEdu.school === "" ||
+      newEdu.eduTitle === "" ||
+      !newEdu.eduStart ||
+      !newEdu.eduEnd
+    ) {
+      alert("Missing Fields");
+      return;
+    }
+    if (newEdu.eduEnd < newEdu.eduStart) {
+      alert("End date must be after start date");
+      setNewEdu((newEdu) => ({
+        ...newEdu,
+        eduEnd: null,
+      }));
+      return;
+    }
+
+    await handleID();
+    setList([...prevList, newEdu]);
+    console.log(`Education saved \n ${JSON.stringify(newEdu)}`);
+  };
 
   return (
     <>
--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,38 +1,39 @@
 import EduForm from "./EduForm";
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
 
 function Education() {
+//   const eduExample = {
+//     school: "Harvard University",
+//     eduTitle: "BSc. Chemistry",
+//     eduStart: "2001-03-25",
+//     eduEnd: "2005-03-25",
+//     eduId: crypto.randomUUID(),
+//   };
 
-  const eduExample = {
-    school: 'Harvard University',
-    eduTitle: "BSc. Chemistry",
-    eduStart: "2001-03-25",
-    eduEnd: "2005-03-25",
-    eduId: uuidv4()
-  }
-
-  const [eduList, setEduList] = useState([eduExample])
-
-
+  const [eduList, setEduList] = useState([]);
 
   function showEduPreview(props) {
-    return (
-      props.eduList.map((edu) => 
-        <div key={edu.eduId}>
-          <p>{edu.school}</p>
-          <p>{edu.eduTitle}</p>
-          <p>{edu.eduStart}</p>
-          <p>{edu.eduEnd}</p>
-        </div>  
-    ))
+    return props.eduList.map((edu) => (
+      <div key={edu.eduId}>
+        <p>{edu.school}</p>
+        <p>{edu.eduTitle}</p>
+        <p>{edu.eduStart}</p>
+        <p>{edu.eduEnd}</p>
+      </div>
+    ));
   }
 
   return (
     <div>
-      <EduForm prevList={eduList} setList={setEduList} isSaved={false} setIsSaved={null} />
+      <EduForm
+        prevList={eduList}
+        setList={setEduList}
+        isSaved={false}
+        setIsSaved={null}
+      />
+      <console className="log">{JSON.stringify(eduList)}</console>
     </div>
-  )
+  );
 }
 
 export default Education;
